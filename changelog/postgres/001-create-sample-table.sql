0a2425a398b6aaa7890abf22071a8c0b61f0cb90
BRLiquibase <briley@liquibase.com>
2026-08-28 15:53:06 +0100

    poc materials

---
+++ changelog/postgres/001-create-sample-table.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset liquibase-poc:001-create-sample-table-postgresql
+CREATE TABLE poc_sample (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+--rollback DROP TABLE poc_sample;
